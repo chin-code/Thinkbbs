e8bdf6bfa54cde6126e965ae79aaf6a0b12e564b
chenzheng <cz17629051320@163.com>
2022-11-19 19:19:33 +0800

    生成目录结构

--- build.php
+++ build.php
@@ -10,17 +10,30 @@
 // +----------------------------------------------------------------------
 
 return [
-    // 生成应用公共文件
-    '__file__' => ['common.php'],
+    // 定义共公模块的自动生成
+    'common'     => [
+        '__file__'   => [],
+        '__dir__'    => ['exception', 'model', 'observer', 'validate'],
+        'controller' => [],
+        'model'      => [],
+        'view'       => [],
+    ],
 
-    // 定义demo模块的自动生成 （按照实际定义的文件名生成）
-    'demo'     => [
-        '__file__'   => ['common.php'],
-        '__dir__'    => ['behavior', 'controller', 'model', 'view'],
-        'controller' => ['Index', 'Test', 'UserType'],
-        'model'      => ['User', 'UserType'],
+    // 定义前台模块的自动生成
+    'index'     => [
+        '__file__'   => [],
+        '__dir__'    => ['config', 'controller', 'index'],
+        'controller' => [],
+        'model'      => [],
         'view'       => ['index/index'],
     ],
 
-    // 其他更多的模块定义
+    // 定义后台模块的自动生成
+    'admin'     => [
+        '__file__'   => [],
+        '__dir__'    => ['config', 'controller', 'index'],
+        'controller' => ['Index'],
+        'model'      => [],
+        'view'       => [],
+    ],
 ];
