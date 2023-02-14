48579dd48b388d9f9ee39cdae7513bec9ef6392c
chenzheng <cz17629051320@163.com>
2023-02-14 14:24:23 +0800

    注册视图表单验证

--- application/common/model/User.php
+++ application/common/model/User.php
@@ -6,5 +6,31 @@ use think\Model;
 
 class User extends Model
 {
-    //
+    /**
+     * @title 验证字段值是否唯一
+     *
+     * Date:2023/2/14 13:52
+     */
+    public static function checkFieldUnique($data,$id = 0)
+    {
+        $field_name = null;
+        if (!isset($data['field'])) {
+            return false;
+        }
+        $field_name = $data['field'];
+        if(!isset($data[$field_name])) {
+            return false;
+        }
+        $field_value = $data[$field_name];
+
+        $query = static::where($field_name, $field_value);
+        if ($id > 0) {
+            $query->where('id','<>',$id);
+        }
+        if ($query->count()) {
+            return false;
+        }
+
+        return true;
+    }
 }
--- application/index/controller/Register.php
+++ application/index/controller/Register.php
@@ -23,11 +23,35 @@ class Register extends Base
      */
     public function save(Request $request)
     {
-        $user = new User;
-        $user->save($request->post());
-        $message = "注册成功";
+        if (!$request->isPost() && !$request->isAjax()) {
+            return $this->error('对不起，你访问的页面不存在。');
+        }
+
+        try {
+            //保存表单提交数据
+            $user = new User;
+            $user->save($request->post());
+        } catch (\Exception $e) {
+            return $this->error('对不起，注册失败。');
+        }
+
         //注册成功后跳转到首页
-        return $this->success($message, '[page.root]');
+        return $this->success('恭喜你注册成功。', '[page.root]');
+    }
+
+    public function check_unique(Request $request)
+    {
+        if (!$request->isAjax()) {
+            return $this->redirect('[page.signup]');
+        }
+
+        $param = $request->post();
+        $is_valid = User::checkFieldUnique($param);
+        if ($is_valid) {
+            echo("true");
+        } else {
+            echo("false");
+        }
     }
 
 
